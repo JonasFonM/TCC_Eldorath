e142edf045aeb6611bd5c546ec01c7b70ba4f2e2
Jonas Fontanella Moroso <prothetbrasil@gmail.com>
2024-09-05 17:25:55 -0300

    Skill Circle shows Pre Requisites

--- app/components/skill-circle.tsx
+++ app/components/skill-circle.tsx
@@ -11,9 +11,19 @@ interface Props {
 }
 
 export function SkillCircle({ skill, isSelected, onClick, isPureLineage }: Props) {
+  const prerequisites = [];
+
+  if (skill.lvl) prerequisites.push(`Level ${skill.lvl}`);
+  if (skill.rlSiz) prerequisites.push(`Relative Size ${skill.rlSiz}`);
+  if (skill.trSiz) prerequisites.push(`True Size ${skill.trSiz}`);
+  if (skill.agi) prerequisites.push(`Agility ${skill.agi}`);
+  if (skill.bdy) prerequisites.push(`Body ${skill.bdy}`);
+  if (skill.mnd) prerequisites.push(`Mind ${skill.mnd}`);
+
   return (
     <div onClick={onClick} style={{ border: isSelected ? '2px solid green' : '1px solid gray', padding: '5%', borderRadius: '2%', backgroundColor: isSelected ? 'rgb(209, 153, 12)' : '' } }>
       <h3 style={{backgroundColor: isPureLineage ? 'gold' : '', color: isPureLineage ? 'black' : 'white'}}>{skill.name}</h3>
+      {prerequisites.length > 0 && <p>Prerequisites: {prerequisites.join(' ')}</p>}
       <p>{skill.techniqueSubtype || skill.type}</p>
       <p>{skill.description}</p>
     </div>

b3309e6fcabcd07b7ef3fe6452578c7817968d38
JonasFonM <prothetbrasil@gmail.com>
2025-02-18 17:35:17 -0300

    First test fetching Campaign data.

--- app/routes/user.campaign.$id.tsx
+++ app/routes/user.campaign.$id.tsx
@@ -1,15 +1,25 @@
+import { campaign } from "@prisma/client";
 import { LoaderFunction } from "@remix-run/node";
-import { Outlet } from "@remix-run/react";
+import { Outlet, useLoaderData } from "@remix-run/react";
+import { prisma } from "~/utils/prisma.server";
 
 export const loader: LoaderFunction = async ({ params }) => {
-    const campaignId = params.id;
-    return (campaignId)
+    const campaignId = Number(params.id);
+
+    const campaign = await prisma.campaign.findUnique({
+        where: { id: campaignId },
+        include: { scenes: true, characters: true, players: true },
+    });
+
+    return ({ campaignId, campaign })
 }
 
 export default function CampaignRoute() {
+    const { campaign } = useLoaderData<{ campaign: campaign }>()
+
     return (
         <div>
-            <h1>Its alive!</h1>
+            <h1>{campaign.title}</h1>
             <Outlet />
         </div>
     );

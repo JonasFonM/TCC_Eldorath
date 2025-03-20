5fc617462707ac72d392d510090be00e302c064e
JonasFonM <prothetbrasil@gmail.com>
2025-03-20 11:55:38 -0300

    Alterando Inventário para ser mais organizado e compacto.

--- app/components/character-sheet/item-display.tsx
+++ app/components/character-sheet/item-display.tsx
@@ -1,4 +1,6 @@
 import { character, character_item, item } from "@prisma/client";
+import React, { useState } from "react";
+import { RenderSlot } from "./render-slots";
 
 
 interface Props {
@@ -12,70 +14,21 @@ export function ItemDisplay({ character, items, onClick }: Props) {
     let totalSlots = Math.max(character.slotAmulet, character.slotBelt, character.slotCloak, character.slotCuirass, character.slotEarings, character.slotGauntlet, character.slotGreaves, character.slotHelm, character.slotPauldron, character.slotRings, character.slotUpperLegs, character.slotWeapon)
     totalSlots % 2 === 0 ? totalSlots++ : totalSlots
 
-    const renderRow = (slots: number[], row: number, totalColumns: number, slotTypes: string[]) => {
-        const items: JSX.Element[] = [];
-        const center = (totalColumns + 1) / 2
-        const columnPlacement: number[][] = slots.map(() => []);
-        let prevMin = center
-        let prevMax = center
-
-        slots.forEach((sl, index) => {
-
-
-            if (index != 0) {
-                prevMin = Math.min(...columnPlacement[index - 1])
-                prevMax = Math.max(...columnPlacement[index - 1])
-            }
-
-            for (let j = 0; j < sl; j++) {
-                index != 0 ?
-                    j % 2 < 1 ?
-                        columnPlacement[index].push(prevMin - 1)
-                        :
-                        columnPlacement[index].push(prevMax + 1)
-                    :
-                    sl > 1 ?
-                        j % 2 < 1 ?
-                            columnPlacement[index].push(center - (Math.floor(j / 2) + 1))
-                            :
-                            columnPlacement[index].push(center + (Math.floor(j / 2) + 1))
-                        :
-                        columnPlacement[index].push(center)
-
-                items.push(
-                    <button key={`${index}-${j}`}
-                        style={{ gridRow: row, gridColumn: columnPlacement[index][j] }}
-                        className={`grid-item`}
-                        onClick={onClick}
-                    >{slotTypes[index].at(4) + ` ${index},${j}`}</button>
-                )
-
-            }
-
-        }
-        )
-        return (items)
-    }
 
     return (
 
         <>
             <div className="inventory"
-                style={{ width: '100%', gridTemplateColumns: `repeat(${totalSlots}, 1fr)` }}>
+                style={{ width: '100%', gridTemplateColumns: `repeat(${7}, 1fr)` }}>
 
-                {renderRow([character.slotHelm, character.slotEarings], 1, totalSlots, ['slotHelm', 'slotEarings'])}
+                <RenderSlot slots={character.slotHelm} slotType="slotHelm" row={1} column={2} onClick={onClick} ></RenderSlot>
+                <RenderSlot slots={character.slotEarings} slotType="slotEarings" row={2} column={2} onClick={onClick} ></RenderSlot>
+                <RenderSlot slots={character.slotCloak} slotType="slotCloak" row={3} column={2} onClick={onClick} ></RenderSlot>
+                <RenderSlot slots={character.slotAmulet} slotType="slotAmulet" row={4} column={2} onClick={onClick} ></RenderSlot>
+                <RenderSlot slots={character.slotRings} slotType="slotRings" row={5} column={2} onClick={onClick} ></RenderSlot>
+                <RenderSlot slots={character.slotWeapon} slotType="slotWeapon" row={6} column={2} onClick={onClick} ></RenderSlot>
 
-                {renderRow([character.slotCuirass, character.slotPauldron, character.slotCloak], 2, totalSlots, ['slotCuirass', 'slotPauldron', 'slotCloak ',])}
 
-                {renderRow([character.slotAmulet, character.slotGauntlet,], 3, totalSlots, ['slotAmulet', 'slotGauntlet'])}
-
-                {renderRow([character.slotRings], 4, totalSlots, ['slotRings'])}
-
-                {renderRow([character.slotWeapon], 5, totalSlots, ['slotWeapon'])}
-
-                {renderRow([character.slotBelt], 6, totalSlots, ['slotBelt'])}
-
-                {renderRow([character.slotUpperLegs, character.slotGreaves], 7, totalSlots, ['slotUpperLegs', 'slotGreaves'])}
 
             </div>
 
